480f8114ac1f2e4c660b036d282999ab8ec5dec0
ymnyho <ymnyho@gmail.com>
2016-01-24 12:48:34 +0800

    add migration

---
+++ database/migrations/2016_01_24_041650_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');   /*會產生 col created_at、update_at*/
+
+            $table->string('title');
+            $table->text('content');
+
+            $table->timestamps();   /*會產生 col id*/
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
